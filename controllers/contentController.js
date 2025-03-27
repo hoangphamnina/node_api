2872f9898c8ccfe28e7a16e4b32e11ea2b7ef6e2
hoangphamnina <hoangpham.tt.nina@gmail.com>
2025-03-27 10:52:58 +0700

    asd as

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -32,12 +32,16 @@ async function CreateContent(req, res) {
             7. Bỏ tiêu đề "Kết luận", "Lời kết", "Mở đầu", "Tóm lại", "Tổng kết",...
             8. Chuyển đổi phần nội dung của content sang dạng json encode để chắc chắn không bị lỗi khi parse json
             9. Không sử dụng nháy đôi (double quotes) trong nội dung json
-            Sau khi hoàn thành, cung cấp SEO Description (160-300 ký tự) và SEO Title (40-70 ký tự) cho bài viết
+            Sau khi hoàn thành, cung cấp cho bài viết:
+            * SEO Description (160-300 ký tự)
+            * SEO Title (40-70 ký tự)
+            * Slug SEO Title
             *Lưu ý: Không cần trả lời gì khác và chỉ hiển thị markdown theo cấu trúc json như bên dưới.
             \`\`\`json
             {
                 \"content\": string (markdown) - Nội dung bài viết,
                 \"title\": string - Seo title,
+                \"slug\": string - Slug seo title,
                 \"description\": string - Seo description
             }
             \`\`\`
